c76a2a5c6694a836c2483537c0e86948a1b91a66
wentywenty <2321901849@qq.com>
2026-05-19 12:13:39 +0800

    update spdx

--- scripts/motion_player.py
+++ scripts/motion_player.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: encoding -*-
 
+# SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2026 Luo1imasi
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import sys
 import os
 import time
